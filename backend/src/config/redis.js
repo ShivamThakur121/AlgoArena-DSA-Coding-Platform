37a07be3c8e692c07f25c297d02bec43f6cc438f
Shivam Thakur <shivamkumr.chk1234@gmail.com>
2026-06-13 01:10:02 +0530

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -5,7 +5,7 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host:  debonair-auric-spot-33541.db.redis.io,
+        host:  'debonair-auric-spot-33541.db.redis.io',
         port: 14070
     }
 });
