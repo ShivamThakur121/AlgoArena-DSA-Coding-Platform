4d302d5227ed0146e40d0963526b4a8774d9f0df
Shivam Thakur <shivamkumr.chk1234@gmail.com>
2026-06-20 15:10:20 +0530

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,19 +1,12 @@
-
-const { createClient }  = require('redis');
+const { createClient } = require('redis');
 
 const redisClient = createClient({
     username: 'default',
     password: 'Vw6oyGRVN01gAg7dFiVrjzkUHlqIfoGL',
     socket: {
-<<<<<<< HEAD
-        host:  'debonair-auric-spot-33541.db.redis.io',
+        host: 'debonair-auric-spot-33541.db.redis.io',
         port: 14070
-=======
-        host: 'redis-11873.c89.us-east-1-3.ec2.cloud.redislabs.com',
-        port: 11873
->>>>>>> 3f69999 (Updated project files)
     }
 });
-
 
 module.exports = redisClient;
