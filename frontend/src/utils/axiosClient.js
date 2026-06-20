9fe2225ac9d6170c7618b6bb5a9414392257c701
Shivam Thakur <shivamkumr.chk1234@gmail.com>
2026-06-20 16:22:08 +0530

    Fix baseURL string formatting in axiosClient

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: https://algoarena-dsa-coding-platform.onrender.com,
+    baseURL: "https://algoarena-dsa-coding-platform.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
