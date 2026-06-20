807deed7d4b90f9587c971ebab2649a34122cdcd
Shivam Thakur <shivamkumr.chk1234@gmail.com>
2026-06-21 01:02:32 +0530

    Update userMiddleware.js

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -1,46 +1,53 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-const redisClient = require("../config/redis")
+const redisClient = require("../config/redis");
 
-const userMiddleware = async (req,res,next)=>{
+const userMiddleware = async (req, res, next) => {
+  try {
 
-    try{
-        
-        const {token} = req.cookies;
-        if(!token)
-            throw new Error("Token is not persent");
+    let token = null;
 
-        const payload = jwt.verify(token,process.env.JWT_KEY);
+    const authHeader = req.headers.authorization;
 
-        const {_id} = payload;
-
-        if(!_id){
-            throw new Error("Invalid token");
-        }
-
-        const result = await User.findById(_id);
-
-        if(!result){
-            throw new Error("User Doesn't Exist");
-        }
-
-        // Redis ke blockList mein persent toh nahi hai
-
-        const IsBlocked = await redisClient.exists(`token:${token}`);
-
-        if(IsBlocked)
-            throw new Error("Invalid Token");
-
-        req.result = result;
-
-
-        next();
-    }
-    catch(err){
-        res.status(401).send("Error: "+ err.message)
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
     }
 
-}
+    if (!token && req.cookies.token) {
+      token = req.cookies.token;
+    }
 
+    if (!token) {
+      throw new Error("Token is not present");
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_KEY);
+
+    const { _id } = payload;
+
+    if (!_id) {
+      throw new Error("Invalid token");
+    }
+
+    const result = await User.findById(_id);
+
+    if (!result) {
+      throw new Error("User Doesn't Exist");
+    }
+
+    const isBlocked = await redisClient.exists(`token:${token}`);
+
+    if (isBlocked) {
+      throw new Error("Invalid Token");
+    }
+
+    req.result = result;
+
+    next();
+
+  } catch (err) {
+    res.status(401).send("Error: " + err.message);
+  }
+};
 
 module.exports = userMiddleware;
